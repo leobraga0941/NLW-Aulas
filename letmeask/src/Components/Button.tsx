e3bf58342de430e6db7291d44a52c94cefc10318
Leonardo <leobraga90@hotmail.com>
2021-06-26 18:17:29 -0300

    Controle de estado de componente

--- letmeask/src/Components/Button.tsx
+++ letmeask/src/Components/Button.tsx
@@ -1,14 +1,32 @@
+/*
 //criação das propriedades do botão
 type ButtonProps = {
   //notação 'variavel: Tipo';
   //inserir '?' na frente das variável deixa essa 'propriedade' opcional
   text?: string;
 }
+*/
+
+import { useState } from "react"
 
 //É Necessário passar o 'props' como parâmetro definindo o tipo declarado anteriormente
-export function Button(props: ButtonProps) {
+//export function Button(props: ButtonProps) {
+export function Button() {
+  //controle de estado => 
+  // Não é uma variável é um array, com dois parâmetros, o primero é a variável
+  // e o segudo a função que altera a variável. Ou seja, o seu estado
+  const [counter, setCounter] = useState(0) //entender pq usou o 0 nesse caso
+  function increment() {
+    //Apesar de imutável o estado é sempre decalarado como constante
+    //O estado é imutável, a alteração acontece nos seus valores
+    setCounter(counter + 1)
+  }
+
   return (
     //todo JS para inserir no HTML utilizar Chaves
-    <button>{props.text || 'Default'}</button>
+    //<button>{props.text || 'Default'}</button>
+    <button onClick={increment}>
+      {counter}
+    </button>
   )
 }
